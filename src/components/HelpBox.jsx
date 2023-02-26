5e609b1313362251dc8183b34ca780921c9cdba3
ToEzBit <jirayut52328172@hotmail.com>
2023-02-26 22:04:54 +0700

    changed h2

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,11 +1,11 @@
-import { string } from 'prop-types';
+import { string } from "prop-types";
 
-import './HelpBox.css';
+import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h3>{title}</h3>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
